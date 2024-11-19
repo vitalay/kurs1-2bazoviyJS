503bfc5f6293313ee3fe03f742bb08e2992e54d0
vitalya.efimov <vitalay.efimov@gmail.com>
2024-11-19 23:51:16 +0500

    дублирую

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-
-let button = document.querySelector('button');
-let resetButton = document.querySelector('.button2');
-let counterText = document.querySelector('.counter');
+let button = document.querySelector("button");
+let resetButton = document.querySelector(".button2");
+let counterText = document.querySelector(".counter");
 let counter = 0;
 
-button.addEventListener('click', () => {
-    counter++;
-    counterText.innerText = counter;
+button.addEventListener("click", () => {
+  counter++;
+  counterText.innerText = counter;
 
-    resetButton.addEventListener('click', () => {
-        counter = 0;
-        counterText.innerText = counter;
-    });
+  resetButton.addEventListener("click", () => {
+    counter = 0;
+    counterText.innerText = counter;
+  });
 });
 // DOM - Document Object Model
-

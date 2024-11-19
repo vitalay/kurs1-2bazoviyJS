72517e605f71167a3320022e9f8a6c010cb72c75
vitalya.efimov <vitalay.efimov@gmail.com>
2024-11-19 23:49:49 +0500

    добавил кнопку 2  сброс счетчика

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 
 let button = document.querySelector('button');
+let resetButton = document.querySelector('.button2');
 let counterText = document.querySelector('.counter');
 let counter = 0;
 
 button.addEventListener('click', () => {
     counter++;
     counterText.innerText = counter;
+
+    resetButton.addEventListener('click', () => {
+        counter = 0;
+        counterText.innerText = counter;
+    });
 });
 // DOM - Document Object Model
+
